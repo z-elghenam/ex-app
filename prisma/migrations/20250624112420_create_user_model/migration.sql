211ec13ee5d18360c3cf8e469a4dd9698a41f3a9
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-06-24 11:24:45 +0000

    Add user role and status enums, and define user model in Prisma schema

---
+++ prisma/migrations/20250624112420_create_user_model/migration.sql
@@ -0,0 +1,32 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('CLIENT', 'AMDIN', 'GUIDE');
+
+-- CreateEnum
+CREATE TYPE "UserStatus" AS ENUM ('ACTIVE', 'INACTIVE', 'SUSPENDED');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "phone" TEXT,
+    "dateOfBirth" TIMESTAMP(3),
+    "profileImage" TEXT,
+    "role" "UserRole" NOT NULL DEFAULT 'CLIENT',
+    "stauts" "UserStatus" NOT NULL DEFAULT 'ACTIVE',
+    "isEmailVerified" BOOLEAN NOT NULL DEFAULT false,
+    "passwordResetToken" TEXT,
+    "passwordResetExpires" TIMESTAMP(3),
+    "emailVerificationToken" TEXT,
+    "emailVerificationExpires" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "lastLoginAt" TIMESTAMP(3),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
