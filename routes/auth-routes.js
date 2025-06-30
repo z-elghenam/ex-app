d5555aecf8e700f0915d7342db6934f0d7e5de5f
ZAKI--V2.0.0 <zelghenam@gmail.com>
2025-06-30 14:34:27 +0000

    feat: Add routes for password reset and email verification

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -4,15 +4,42 @@ const {
   login,
   getUserProfile,
   verifyEmail,
+  forgotPassword,
+  resetPassword,
 } = require("../controllers/auth-controller");
 const { auth } = require("../middleware/auth");
 
 const router = express.Router();
 
+
+// @route POST /api/auth/register
+// @desc register user
+// @access Public
 router.post("/register", register);
+
+// @route POST /api/auth/login
+// @desc Login user
+// @access Public
 router.post("/login", login);
+
+// @route GET /api/auth/verify-email?token=*****
+// @desc Verify email with token
+// @access Public
 router.get("/verify-email", verifyEmail);
 
+// @route POST /api/auth/forgot-password
+// @desc Send password reset email
+// @access Public
+router.post('/forgot-password', forgotPassword)
+
+// @route POST /api/auth/reset-password?token=*****
+// @desc Reset password with token
+// @access Public
+router.post('/reset-password', resetPassword)
+
+// @route GET /api/auth/me
+// @desc Get current user
+// @access Private
 router.get("/me", auth, getUserProfile);
 
 module.exports = router;
